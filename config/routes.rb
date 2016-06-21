83a1261642a06b14495c8ce8a1740b4da944101d
Jacquelyn Ma <jacquelyn@unbounce.com>
2016-06-20 21:48:34 -0700

    Adding Party model Parties are added to the guestlist & represent the person or group people who are being invited as a guest.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :parties
+
   resources :people
 
   get 'welcome/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610170203) do
+ActiveRecord::Schema.define(version: 20160621044756) do
 
   create_table "guestlists", force: true do |t|
     t.string   "title"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160610170203) do
   end
 
   create_table "guests", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "parties", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
